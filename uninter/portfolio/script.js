a889bb57953dc22e8c7da06dba025786c3671de0
Georg <rybskigeorg@gmail.com>
2024-05-27 22:44:27 -0300

    chore: Add required comments in pt-BR

--- uninter/portfolio/script.js
+++ uninter/portfolio/script.js
@@ -1,12 +1,15 @@
 function openTab(tabName) {
     var i, tabcontent;
+    // Recupera todos os elementos de conteúdo de aba
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
+    // Mostra o conteúdo da aba selecionada
     document.getElementById(tabName).style.display = "block";
 }
 
 window.onload = function() {
-    openTab('about'); // Initialize with the About Me tab open
+    // Inicializa com a aba 'Sobre Mim' aberta
+    openTab('about');
 };
